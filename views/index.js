768ae65a26023c1a778c91839e147a1e36536414
YuriyLisovskiy <yuralisovskiy98@gmail.com>
2019-02-21 01:41:25 +0200

    Moved html documents to static/html

--- views/index.js
+++ views/index.js
@@ -2,6 +2,6 @@ let path = require('path');
 
 module.exports = {
 	Index: async function (request, response) {
-		response.sendFile(path.resolve('static/index.html'));
+		response.sendFile(path.resolve('static/html/index.html'));
 	}
 };
