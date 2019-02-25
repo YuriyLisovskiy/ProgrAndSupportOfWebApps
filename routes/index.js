0f6e40cd5528126e413eff8ab66d710c49e537f3
YuriyLisovskiy <yuralisovskiy98@gmail.com>
2019-02-25 23:41:57 +0200

    Minor changes

--- routes/index.js
+++ routes/index.js
@@ -8,13 +8,16 @@ let AdministrationView = require('../views/administration');
 
 router.get('/', IndexView.Index);
 router.get('/logout', AuthViews.Logout);
-router.get('/api/goods', GoodsView.Goods);
 router.get('/administration', AdministrationView.Administration);
+
+router.post('/administration', AdministrationView.Administration);
+
+
+router.get('/api/goods', GoodsView.Goods);
 
 router.post('/api/login', AuthViews.Login);
 router.post('/api/goods', GoodsView.Goods);
 router.post('/api/register', AuthViews.Register);
 router.post('/api/token/verify', AuthViews.VerifyToken);
-router.post('/administration', AdministrationView.Administration);
 
 module.exports = router;
--- scripts/shop.sql
+++ scripts/shop.sql
@@ -1,6 +1,14 @@
 CREATE TABLE Users (
-  email         VARCHAR(255),
+  email         VARCHAR(255)  NOT NULL,
   username      VARCHAR(100)  PRIMARY KEY,
-  password      VARCHAR(100),
-  is_superuser  BOOLEAN DEFAULT FALSE
+  password      VARCHAR(100)  NOT NULL,
+  is_superuser  BOOLEAN       DEFAULT FALSE
+);
+
+CREATE TABLE Goods (
+  code          INTEGER       PRIMARY KEY,
+  title         VARCHAR(255)  NOT NULL,
+  price         DECIMAL       NOT NULL,
+  image         VARCHAR(500)  NULL,
+  description   TEXT DEFAULT  NULL
 );
